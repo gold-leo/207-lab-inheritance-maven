6ac9193e1c0d222ff79e5ef825e99fdaa08412b4
gold-leo <goldmanleo@grinnell.edu>
2024-10-02 18:31:12 -0500

    3

---
+++ src/main/java/edu/grinnell/csc207/util/DecrementableCounter.java
@@ -0,0 +1,23 @@
+package edu.grinnell.csc207.util;
+
+/**
+ * Counters that you can decrement.
+ */
+public class DecrementableCounter extends BasicCounter {
+  /**
+   * Create a new decrementable counter.
+   *
+   * @param start
+   *   The initial value of the counter.
+   */
+  public DecrementableCounter(int start) {
+    super(start);
+  } // DecrementableCounter(int)
+
+  /**
+   * Decrements the counter.
+   */
+  public void decrement() {
+    this.count -= 1;
+  } // decrement()
+} // class DecrementableCounter
