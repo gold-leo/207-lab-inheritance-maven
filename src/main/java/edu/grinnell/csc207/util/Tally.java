62b11754df3a7ded84f990875989cb56493ae22c
gold-leo <goldmanleo@grinnell.edu>
2024-10-02 18:25:59 -0500

    1

---
+++ src/main/java/edu/grinnell/csc207/util/Tally.java
@@ -0,0 +1,18 @@
+package edu.grinnell.csc207.util;
+
+/**
+ * Objects that Tally values.
+ */
+public class Tally extends BasicCounter {
+  /**
+   * Create a new Tally.
+   *
+   * @param start
+   *   The initial value of the tally.
+   */
+  public Tally() {
+    super(0);
+  } // Tally(int)
+
+
+} // class Tally
